999e97698c1dced4c7ba72193196ff4287cf491f
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-02-08 14:37:51 +0000

    adds functionality for search api call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import "./index.css";
 import { useState, useEffect } from "react";
-import Header from 
-import SearchBar from 
-import MovieList from 
+import { getAllMovies, getSearchedMovies } from "./api.js";
+import Header from "./components/Header.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import MovieList from "./components/MovieList.jsx";
 
 function App() {
-  const [searchValue, setSearchValue] = useState("")
-  const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue] = useState("");
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    getAllMovies().then((moviesFromAPI) => {
+      if (searchValue === "") {
+        setMovies(moviesFromAPI);
+      } else if (searchValue.length > 1) {
+        getSearchedMovies(searchValue).then((searchResults) => {
+          setMovies(searchResults);
+        });
+      }
+    });
+  }, [searchValue]);
 
-
-  return <div className='App'>
-    <Header title="Ste's Movies"></Header>
-    <SearchBar setSearchValue={setSearchValue}></SearchBar>
-    <MovieList movies={movies}></MovieList>
-  </div>;
+  return (
+    <div className='App'>
+      <Header title="Ste's Movies"></Header>
+      <SearchBar setSearchValue={setSearchValue}></SearchBar>
+      <MovieList movies={movies}></MovieList>
+    </div>
+  );
 }
 
 export default App;
