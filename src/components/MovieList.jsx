49f6055a0b86a179c662c0d1e7135d0fb12091fd
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-02-08 14:37:14 +0000

    adds poster image and rendering capabilities

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,18 @@
 const MovieList = ({ movies }) => {
+  const imgsrc = "https://image.tmdb.org/t/p/w1280";
+
   return (
     <div className='movie-list'>
       {movies.map((movie) => (
-        <div className='moviecard' key={movie.id}></div>
+        <div className='moviecard' key={movie.id}>
+          <h1>{movie.title}</h1>
+          <img
+            src={imgsrc + movie.poster_path}
+            alt='movie-poster'
+            width={200}
+            height={300}
+          />
+        </div>
       ))}
     </div>
   );
