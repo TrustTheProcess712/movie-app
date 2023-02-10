1a429a9eff32912cd9baf412d7557bc7bd093759
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-02-10 18:23:05 +0000

    adds css to movie details app needs more refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-
+// import SearchBar from "./SearchBar.jsx";
 const Header = ({ title }) => {
   return (
     <div className='header'>
-      <Link to='/movies'>
+      <Link to='/movie'>
         <h1>{title}</h1>
       </Link>
+      {/* <SearchBar>{children}</SearchBar> */}
     </div>
   );
 };
