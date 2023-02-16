83d2dd33206fc1d50ebd6bd2bc215e65f5547b84
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-02-16 12:32:39 +0000

    adds basic error and loading handling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,43 +5,46 @@ import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
 import Grid from "@mui/material/Grid";
 
-const MovieCard = ({ movies }) => {
+const MovieCard = ({ movies, loading, error }) => {
   const imgsrc = "https://image.tmdb.org/t/p/w1280";
 
-  // console.log(movies);
-
+  console.log(error);
   return (
     <>
-      {movies.map((movie) => {
-        return (
-          <Grid key={movie.id} item xs={3}>
-            {/* <h3>{movie.title}</h3> */}
-            <Card
-              square
-              elevation={10}
-              // variant='outlined'
-              sx={{
-                maxWidth: 200,
-                minWidth: 200,
-                maxHeight: 300,
-                marginBottom: "1.5rem",
-                marginLeft: "2.25rem",
-              }}>
-              <CardActionArea>
-                <Link to={`/movie/${movie.id}`}>
-                  <CardMedia
-                    component='img'
-                    image={imgsrc + movie.poster_path}
-                    width={200}
-                    height={300}
-                    alt='movie-poster'
-                  />
-                </Link>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        );
-      })}
+      {error && <p>{error}</p>}
+      {loading && <h1>Loading...</h1>}
+      {!loading &&
+        !error &&
+        movies.map((movie) => {
+          return (
+            <Grid key={movie.id} item xs={3}>
+              {/* <h3>{movie.title}</h3> */}
+              <Card
+                square
+                elevation={10}
+                // variant='outlined'
+                sx={{
+                  maxWidth: 200,
+                  minWidth: 200,
+                  maxHeight: 300,
+                  marginBottom: "1.5rem",
+                  marginLeft: "2.25rem",
+                }}>
+                <CardActionArea>
+                  <Link to={`/movie/${movie.id}`}>
+                    <CardMedia
+                      component='img'
+                      image={imgsrc + movie.poster_path}
+                      width={200}
+                      height={300}
+                      alt='movie-poster'
+                    />
+                  </Link>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
     </>
   );
 };
